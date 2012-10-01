ea59dd88ae41fc3a5acaea5bfc77f5631d25e2f1
Omkar <hiomkar@gmail.com>
2012-10-01 15:50:57 -0400

    adding seed data for subcategories

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 
 
 categories = Category.create([{ name: 'Electronics' }, { name: 'Books' }, { name: 'Appliances' }, { name: 'Furniture' }])
+
+subcategories = Subcategory.create([{ category_id: '1', name: 'Laptops' }, { category_id: '1', name: 'Cameras' }, { category_id: '1', name: 'Mobiles' }, { category_id: '1', name: 'Headphones' }, { category_id: '2', name: 'Fiction' }, { category_id: '2', name: 'Non-Fiction' }, { category_id: '3', name: 'Tables' }, { category_id: '3', name: 'Chairs' }])
