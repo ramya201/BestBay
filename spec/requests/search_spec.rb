8fd1e526837e24a2f7428478169abfa1451eb813
Omkar <hiomkar@gmail.com>
2012-11-10 01:21:15 -0500

    fixing test cases

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe "Item Search" do
 
   it "should list search results based on keywords" do
-    item = FactoryGirl.create(:item, :name => "Kodak camera 10 megapixel")
+    @user_attr = FactoryGirl.attributes_for(:user)
+    user = User.create!(@user_attr)
+    item = FactoryGirl.create(:item, :name => "Kodak camera 10 megapixel", :user => user)
     visit "/welcome/index"
     fill_in('search_keyword', :with => 'Kodak')
     click_button "search_submit"
