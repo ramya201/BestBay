1c303ed44806b28513907a4e54dd586d91152909
mrhooray <hurui900313@gmail.com>
2012-11-19 20:56:49 -0500

    finish payment for instant sale

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,21 +1,43 @@
 class CartController < ApplicationController
 	include CartHelper
 
-	def add                                                            #adds items to cart
+	def add
 		add_to_cart(params[:item_obj], params[:qty])
 		redirect_to :controller => "items", :action => "show"
 	end
 
-	def get                                                            #shows items in the cart
-		get_all_items_in_cart
+	def get
 		redirect_to :controller => "items", :action => "show"
 	end
 
-  def remove
-    remove_from_cart(Item.find(params[:id]))
-    redirect_to :controller => "cart", :action => "index"
-  end
+	def remove
+		remove_from_cart(Item.find(params[:id]))
+		redirect_to :controller => "cart", :action => "index"
+	end
 
 	def index
+	end
+	
+	def checkout
+	end
+
+	def pay
+		items=get_all_items_in_cart
+		to_be_removed=Array.new
+		items.each do |cart_item|
+			item=Item.find(cart_item.fetch("table").fetch("cart_item_id"))
+			if item.quantity<cart_item.fetch("table").fetch("count")
+				flash[:notice] = "inventory not enough for item: #{item.name}"
+				redirect_to :controller => "cart", :action => "index"
+			end
+			item.quantity-=cart_item.fetch("table").fetch("count")
+			item.save
+			to_be_removed.append(item)
+		end
+		to_be_removed.each do |rm_item|
+			remove_from_cart(rm_item)
+		end
+		flash[:notice] = "checkout successfully"
+		redirect_to :controller => "categories", :action => "index"
 	end
 end
